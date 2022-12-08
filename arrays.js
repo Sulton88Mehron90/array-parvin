6cf45fef07a1ad6621404c6c3ac2252a2afc39ec
Parvin Sattorova <nanajon66@gmail.com>
2022-12-08 02:20:51 -0500

    Add exsplonation on the array method used

--- arrays.js
+++ arrays.js
@@ -1,5 +1,33 @@
 var appleNames = ["Cripps Pink", "Honeycrisp", "Granny Smith", "Gala"]
+appleNames.pop()
+console.log(appleNames);
+
+```
+parvinsattorovaedwards@Parvins-MacBook-Pro array % node arrays.js 
+[ 'Cripps Pink', 'Honeycrisp', 'Granny Smith' ]
+```
+// used `pop()` method – Remove an element from the end of the array.
 
 var birthDates = ["3", "6", "21", "17", "10"]
+birthDates.push()
+console.log(birthDates)
+
+```
+parvinsattorovaedwards@Parvins-MacBook-Pro array % node arrays.js
+[ 'Cripps Pink', 'Honeycrisp', 'Granny Smith' ]
+[ '3', '6', '21', '17', '10' ]
+```
+// used `push()` method - – Insert an element at the end of the array.
 
 var fourBooleans = ["true", "false", "yes", "no"]
+fourBooleans.shift()
+console.log(fourBooleans)
+
+```
+parvinsattorovaedwards@Parvins-MacBook-Pro array % node arrays.js
+[ 'Cripps Pink', 'Honeycrisp', 'Granny Smith' ]
+[ '3', '6', '21', '17', '10' ]
+[ 'false', 'yes', 'no' ]
+parvinsattorovaedwards@Parvins-MacBook-Pro array %
+```
+// used shift() – Remove an element from the beginning of the array.
